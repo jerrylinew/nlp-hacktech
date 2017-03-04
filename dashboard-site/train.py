a27f95ab61c07fb5a4df5dd96d63e6041d8c52be
kylewong975 <kyle.wong975@gmail.com>
2017-03-04 15:06:14 -0800

    Remove dummy python script from webpage

--- dashboard-site/train.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-data =  {"id": "nlp-hacktech",
- "storageDataLocation": "nlp-hacktech/train.csv"}
-
-r = requests.post('https://www.googleapis.com/prediction/v1.6/projects/nlp-hacktech/trainedmodels', data)
-
-print(r.json)
