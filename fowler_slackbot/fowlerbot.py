135d3707b42d94c6760eefcece1417c8cab30604
Jayendra <jay08jog@gmail.com>
2017-03-04 22:27:19 -0800

    theres about to be hella merge conflicts

--- fowler_slackbot/fowlerbot.py
+++ fowler_slackbot/fowlerbot.py
@@ -7,6 +7,9 @@ import argparse
 import pprint
 import sys
 import sqlite3
+import httplib
+
+import requests
 
 from apiclient import discovery
 from apiclient import sample_tools
@@ -41,15 +44,44 @@ def db_conn():
 		"schema": schema,
 		"fileout": fileout
 	}
+	sql.execute("SELECT user, num_violations FROM " + sqltable + " ORDER BY num_violations DESC")
+	output_data = sql.fetchall()
+	output_data = output_data[:5]
+	print output_data
+	db_post_output(output_data)
+	return dbconfig
+
+def db2_conn():
+	sqldb = "user.db"
+	sqltable = "accusations"
+	schema = "(user, userid, num_violations)"
+	fileout = "./accusations.csv"
+	conn = sqlite3.connect(sqldb)
+	sql = conn.cursor()
+	sql.execute("CREATE TABLE IF NOT EXISTS " + sqltable + " " + schema)
+	dbconfig = {
+		"conn": conn,
+		"sql": sql,
+		"sqltable": sqltable,
+		"schema": schema,
+		"fileout": fileout
+	}
+	sql.execute("SELECT user, num_violations FROM " + sqltable + " ORDER BY num_violations DESC")
+	output_data = sql.fetchall()
+	output_data = output_data[:5]
+	print output_data
+	db_post_output2(output_data)
 	return dbconfig
 
 def db_post_output(output_data):
 	try:
-		conn = httplib.HTTPSConnection('localhost')
-		conn.request("POST", "/", output_data, headers)
-		response = conn.getresponse()
-		data = json.loads(response.read())
-		conn.close()
+		r = requests.post('http://localhost:3000/data', data={'data': output_data})
+	except Exception as e:
+		print e
+
+def db_post_output2(output_data):
+	try:
+		r = requests.post('http://localhost:3000/reported', data={'data': output_data})
 	except Exception as e:
 		print e
 
@@ -75,6 +107,29 @@ def db_execute(dbconfig, user, userid):
 	output_data = output_data[:5]
 	print output_data
 	db_post_output(output_data)
+
+def db_accusations(dbconfig, user, userid):
+	conn = dbconfig["conn"]
+	sql = dbconfig["sql"]
+	sqltable = dbconfig["sqltable"]
+	sql.execute("SELECT * FROM " + sqltable + " WHERE userid='" + userid.encode('ascii') + "'")
+	user_data = sql.fetchone()
+	sql.execute("DELETE FROM " + sqltable + " WHERE userid='" + userid.encode('ascii') + "'")
+	if user_data == None:
+		user_data = (user.encode('ascii'),userid.encode('ascii'),1)
+		
+	user_data = (user_data[0].encode('ascii'),user_data[1].encode('ascii'),user_data[2]+1)
+	sql.execute("INSERT INTO " + sqltable + " VALUES " + str(user_data))
+	
+	print user_data
+	conn.commit()
+	
+	#get top 5
+	sql.execute("SELECT user, num_violations FROM " + sqltable + " ORDER BY num_violations DESC")
+	output_data = sql.fetchall()
+	output_data = output_data[:5]
+	print output_data
+	db_post_output2(output_data)
 
 def print_header(line):
 	'''Format and print header block sized to length of line'''
@@ -121,7 +176,6 @@ def handle_command(user, userid,  text, service, flags, channel, dbconfig):
 		
 		db_execute(dbconfig, user, userid)
 
-
 def parse_slack_output(slack_rtm_output):
 	output_list = slack_rtm_output
 	if output_list and len(output_list) > 0:
@@ -140,8 +194,18 @@ def parse_slack_output(slack_rtm_output):
 				pass
 	return None, None, None, None
 
+def sexual_harassment_complaint(text, userid, dbconfig):
+	for member in USERS['members']:
+		try:
+			if member['name'] in text:
+				print member['name'] + " is accused of sexual harassment"
+				db_accusations(dbconfig, member['name'], member['id'])
+		except Exception:
+			pass
+
 def main(argv):
 	dbconfig = db_conn()
+	dbconfig2 = db2_conn()
 	READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
 	service, flags = sample_tools.init(
 		argv, 'prediction', 'v1.6', __doc__, __file__, parents=[argparser],
@@ -152,7 +216,9 @@ def main(argv):
 		print("FowlerBot connected and running!")
 		while True:
 			user, userid,  text, channel = parse_slack_output(slack_client.rtm_read())
-			if user and text:
+			if channel == 'D4D3GBG0Z':
+				sexual_harassment_complaint(text, userid, dbconfig2)
+			elif user and text:
 				handle_command(user, userid, text, service, flags, channel, dbconfig)
 			time.sleep(READ_WEBSOCKET_DELAY)
 	else:
